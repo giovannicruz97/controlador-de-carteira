d5b4fef5d6ada924312045af0ba07496107805c7
Giovanni Cruz <giocruz@purple-rain.localdomain>
2021-12-26 19:34:48 -0300

    feat: adding initial domains

---
+++ src/Type.ts
@@ -0,0 +1,7 @@
+export enum Type {
+    STOCK = 'stock',
+    ETF = 'ETF',
+    REAL_ESTATE_INVESTIMENT_FUND = 'real estate investiment fund',
+    STOCK_INVESTIMENT_FUND = 'stock investiment fund',
+    CRYPTOCURRENCY = 'cryptocurrency'
+}
