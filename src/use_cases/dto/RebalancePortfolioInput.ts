3201fa9f3a6df45dc3be75e89fff1a92dd23d731
giovannicruz97 <giocruz.it@gmail.com>
2022-01-16 13:14:01 -0300

    feat: adding contribution input

--- src/use_cases/dto/RebalancePortfolioInput.ts
+++ src/use_cases/dto/RebalancePortfolioInput.ts
@@ -1,11 +1,15 @@
-export default class RebalancePortfolioInput {
-    public ticker: string;
-    public currentQuantity: number;
-    public targetAllocationPercentage: number;
+interface Asset {
+    ticker: string;
+    currentQuantity: number;
+    targetAllocationPercentage: number;
+};
 
-    public constructor({ ticker, currentQuantity, targetAllocationPercentage }: { ticker: string; currentQuantity: number; targetAllocationPercentage: number }) {
-        this.ticker = ticker;
-        this.currentQuantity = currentQuantity;
-        this.targetAllocationPercentage = targetAllocationPercentage;
+export default class RebalancePortfolioInput {
+    public assets: Asset[];
+    public contribution: number;
+
+    public constructor({ assets, contribution = 0 }: { assets: Asset[]; contribution: number }) {
+        this.assets = assets;
+        this.contribution = contribution;
     }
 }
